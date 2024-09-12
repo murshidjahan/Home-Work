fb67fe0811933d8346a4c04d3662b63d1f4b9262
Murshid Jahan <105840903+Murshed18@users.noreply.github.com>
2024-09-12 21:05:27 +0600

    Create simple_calculator.py

---
+++ simple_calculator.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter first number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    print(num1 - num2)
+elif operator == '*':
+    print(num1 * num2)
+elif operator == '/':
+    if num2 != 0:
+        print(num1 / num2)
+    else:
+        print("Division by zero is not allowed.")
+else:
+    print("Invalid Number or operator. Please enter one of +, -, *, /.")
